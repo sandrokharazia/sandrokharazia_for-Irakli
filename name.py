81fe06b8e8132c080e060571e6a82d69445f16dd
sandrokharazia <kharaziasandro1@gmail.com>
2025-03-08 23:23:29 +0400

    Upload name.py file
    This script handles user input and processes data

---
+++ name.py
@@ -0,0 +1,12 @@
+'''
+c = input("if you want to get in club tell me your name first! ")
+print("ok now tell me your age ", c, "!")
+c = int(input("how old are you? "))
+
+
+if c < 18:                 
+    print("you can't go")
+else:
+    print("you can go")
+    
+'''
